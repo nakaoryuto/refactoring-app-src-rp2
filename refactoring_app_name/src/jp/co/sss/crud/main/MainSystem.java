97980abaa244bdd53881ba560f4299bd570c6b27
r-nakao <nakaoryuto.tis2025@gmail.com>
2026-01-13 17:17:47 +0900

    fix:MainSystem.javaの変数名の変更
    狙い：可読性の向上
    以下の変数を中身が何か明確に表す名前に変更。
    br        → consoleReader
    
    menuNo    → selectedMenuNumber
    
    menuNoStr → selectedMenuNumberString
    
    deptIdA   → departmentIdString
    
    deptIdB   → departmentIdStringForInsert
    
    emp_name  → employeeName
    
    Seibetsu  → genderCodeString
    
    birthday  → birthdayString
    
    empId_1   → employeeIdStringForUpdate

--- refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
+++ refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
@@ -19,15 +19,15 @@ public class MainSystem {
 	/**
 	 * 社員管理システムを起動
 	 *
-	 * @throws IOException 
-	 * @throws SQLException 
-	 * @throws ClassNotFoundException 
-	 * @throws ParseException 
+	 * @throws IOException
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 * @throws ParseException
 	 */
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, ParseException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
-		int menuNo = 0;
+		int selectedMenuNumber = 0;
 
 		do {
 			// メニューの表示
@@ -42,72 +42,61 @@ public class MainSystem {
 			System.out.print("メニュー番号を入力してください：");
 
 			// メニュー番号の入力
-			String menuNoStr = br.readLine();
-			menuNo = Integer.parseInt(menuNoStr);
+			String selectedMenuNumberString = consoleReader.readLine();
+			selectedMenuNumber = Integer.parseInt(selectedMenuNumberString);
 
 			// 機能の呼出
-			switch (menuNo) {
+			switch (selectedMenuNumber) {
 			case 1:
-				// 全件表示機能の呼出
-				DBController.find();
+				DBController.findAllEmployees();
 				break;
 
 			case 2:
-				// 社員名検索
 				System.out.print("社員名:");
-
-				// 検索機能の呼出
-				DBController.findB();
+				DBController.findEmployeesByNameFromConsole();
 				break;
 
 			case 3:
-				// 検索する部署IDを入力
 				System.out.print("部署ID(1:営業部、2:経理部、3:総務部)を入力してください:");
-				String deptIdA = br.readLine();
-
-				// 検索機能の呼出
-				DBController.findC(deptIdA);
+				String departmentIdString = consoleReader.readLine();
+				DBController.findEmployeesByDepartmentId(departmentIdString);
 				break;
 
 			case 4:
-				// 登録する値を入力
 				System.out.print("社員名:");
-				String emp_name = br.readLine();
-				System.out.print("性別(0:その他, 1:男性, 2:女性, 9:回答なし):");
-				String Seibetsu = br.readLine();
-				System.out.print("生年月日(西暦年/月/日):");
-				String birthday = br.readLine();
-				System.out.print("部署ID(1:営業部、2:経理部、3:総務部):");
-				String deptIdB = br.readLine();
+				String employeeName = consoleReader.readLine();
 
-				// 登録機能の呼出
-				DBController.insert(emp_name, Seibetsu, birthday, deptIdB);
+				System.out.print("性別(0:その他, 1:男性, 2:女性, 9:回答なし):");
+				String genderCodeString = consoleReader.readLine();
+
+				System.out.print("生年月日(西暦年/月/日):");
+				String birthdayString = consoleReader.readLine();
+
+				System.out.print("部署ID(1:営業部、2:経理部、3:総務部):");
+				String departmentIdStringForInsert = consoleReader.readLine();
+
+				DBController.insertEmployee(employeeName, genderCodeString, birthdayString,
+						departmentIdStringForInsert);
 				break;
 
 			case 5:
-				// 更新する社員IDを入力
 				System.out.print("更新する社員の社員IDを入力してください：");
+				String employeeIdStringForUpdate = consoleReader.readLine();
 
-				// 更新する値を入力する
-				String empId_1 = br.readLine();
-				Integer.parseInt(empId_1);
+				Integer.parseInt(employeeIdStringForUpdate);
 
-				// 更新機能の呼出
-				DBController.update(empId_1);
+				DBController.updateEmployeeFromConsole(employeeIdStringForUpdate);
 				System.out.println("社員情報を更新しました");
-
 				break;
 
 			case 6:
-				// 削除する社員IDを入力
 				System.out.print("削除する社員の社員IDを入力してください：");
-
-				// 削除機能の呼出
-				DBController.delete();
+				DBController.deleteEmployeeFromConsole();
 				break;
 
 			}
-		} while (menuNo != 7);
+		} while (selectedMenuNumber != 7);
+
 		System.out.println("システムを終了します。");
 	}
 }
